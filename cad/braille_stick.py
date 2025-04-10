89830e0a26eeebc925621430797aebd986d887ad
KilowattSynthesis <180148956+KilowattSynthesis@users.noreply.github.com>
2025-04-09 23:47:05 -0600

    Implement braille logic, finish stick

--- cad/braille_stick.py
+++ cad/braille_stick.py
@@ -2,6 +2,7 @@ import json
 import math
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Literal
 
 import build123d as bd
 from build123d_ease import show
@@ -18,6 +19,194 @@ from loguru import logger
 # Do a bitwise OR together for all raised dots, like:
 # (1 << (dot_number - 1)) | (1 << (dot_number - 1)) | ...
 
+# Mapping Source: https://stackoverflow.com/questions/41922629/convert-text-to-braille-unicode-in-python
+ASCII_TO_BRAILLE_UNICODE_MAPPING = {
+    " ": "⠀",  # Note: Key is a space. Value is not a space.
+    "!": "⠮",
+    '"': "⠐",
+    "#": "⠼",
+    "$": "⠫",
+    "%": "⠩",
+    "&": "⠯",
+    "": "⠄",
+    "(": "⠷",
+    ")": "⠾",
+    "*": "⠡",
+    "+": "⠬",
+    ",": "⠠",
+    "-": "⠤",
+    ".": "⠨",
+    "/": "⠌",
+    "0": "⠴",
+    "1": "⠂",
+    "2": "⠆",
+    "3": "⠒",
+    "4": "⠲",
+    "5": "⠢",
+    "6": "⠖",
+    "7": "⠶",
+    "8": "⠦",
+    "9": "⠔",
+    ":": "⠱",
+    ";": "⠰",
+    "<": "⠣",
+    "=": "⠿",
+    ">": "⠜",
+    "?": "⠹",
+    "@": "⠈",
+    "a": "⠁",
+    "b": "⠃",
+    "c": "⠉",
+    "d": "⠙",
+    "e": "⠑",
+    "f": "⠋",
+    "g": "⠛",
+    "h": "⠓",
+    "i": "⠊",
+    "j": "⠚",
+    "k": "⠅",
+    "l": "⠇",
+    "m": "⠍",
+    "n": "⠝",
+    "o": "⠕",
+    "p": "⠏",
+    "q": "⠟",
+    "r": "⠗",
+    "s": "⠎",
+    "t": "⠞",
+    "u": "⠥",
+    "v": "⠧",
+    "w": "⠺",
+    "x": "⠭",
+    "y": "⠽",
+    "z": "⠵",
+    "[": "⠪",
+    "\\": "⠳",
+    "]": "⠻",
+    "^": "⠘",
+    "_": "⠸",
+}
+assert len(set(ASCII_TO_BRAILLE_UNICODE_MAPPING.keys())) == len(
+    ASCII_TO_BRAILLE_UNICODE_MAPPING
+), "Braille unicode mapping must be unique! Keys have duplicates."
+assert len(set(ASCII_TO_BRAILLE_UNICODE_MAPPING.values())) == len(
+    ASCII_TO_BRAILLE_UNICODE_MAPPING
+), "Braille unicode mapping must be unique! Values have duplicates."
+
+
+class BrailleCell:
+    """Representation of a single braille cell."""
+
+    def __init__(self, cell_input: str | int) -> None:
+        """Create a braille cell.
+
+        Args:
+            cell_input (str | int): Braille cell representation. Can be either
+                a single character, or an int representing the braille cell in
+                binary, or a braille unicode character.
+
+        """
+        if isinstance(cell_input, str):
+            if len(cell_input) != 1:
+                msg = "String input must be a single character."
+                raise ValueError(msg)
+
+            # If it's in the 0x2800 to 0x28FF range, it's a braille unicode
+            # character.
+            if ord(cell_input) in range(0x2800, 0x28FF + 1):
+                _cell_int_repr = ord(cell_input) - 0x2800
+            else:
+                # Convert the character to its braille unicode representation.
+                cell_unicode_char = ASCII_TO_BRAILLE_UNICODE_MAPPING[
+                    # TODO(KilowattSynthesis): Implement case-sensitive logic.
+                    cell_input.lower()
+                ]
+                _cell_int_repr = ord(cell_unicode_char) - 0x2800
+
+        elif isinstance(cell_input, int):
+            _cell_int_repr = cell_input
+        else:
+            msg = "Input must be a string or an integer."
+            raise TypeError(msg)
+
+        # Set attribute(s).
+        self.cell_int_repr: int = _cell_int_repr
+
+    @property
+    def cell_unicode_character(self) -> str:
+        """Get the unicode character representation of the braille cell."""
+        return chr(self.cell_int_repr + 0x2800)
+
+    @property
+    def cell_ascii_character(self) -> str:
+        """Get the ASCII character representation of the braille cell."""
+        for (
+            ascii_char,
+            braille_char,
+        ) in ASCII_TO_BRAILLE_UNICODE_MAPPING.items():
+            if braille_char == self.cell_unicode_character:
+                return ascii_char
+
+        # If we get here, the cell is not in the mapping.
+        msg = (
+            "ASCII character not found for braille cell "
+            f"|{self.cell_unicode_character}|"
+        )
+        raise ValueError(msg)
+
+    @property
+    def dot_count(self) -> Literal[6, 8]:
+        """Get the "type" of braille cell (either 6-dot or 8-dot)."""
+        if self.cell_int_repr > 0b111111:  # noqa: PLR2004
+            return 8
+        return 6
+
+    def get_raised_xy_point_list(
+        self, dots_per_cell_mode: Literal[6, 8]
+    ) -> list[tuple[float, float]]:
+        """Get the XY coordinates of the raised dots in the braille cell.
+
+        Args:
+            dots_per_cell_mode (Literal[6, 8]): The number of dots per cell
+                (either 6 or 8).
+
+        Returns:
+            list[tuple[float, float]]: A list of tuples representing the XY
+                coordinates of the raised dots. Assuming the dots are all
+                "1 unit" apart. In 6-dot mode, the dots are arranged in a 2x3
+                grid with the middle dots at Y=0. In 8-dot mode, the dots
+                are arranged in a 2x4 grid with the inner dots at Y=0.5 and
+                Y=-0.5. Top-left corner is (-X, +Y), like Quadrant 1.
+
+        """
+        if dots_per_cell_mode not in (6, 8):
+            msg = "Invalid braille cell mode. Must be either 6 or 8."
+            raise ValueError(msg)
+
+        dot_to_coord_mapping_mode_6 = {
+            1: (-0.5, 1.0),
+            2: (-0.5, 0.0),
+            3: (-0.5, -1.0),
+            4: (0.5, 1.0),
+            5: (0.5, 0.0),
+            6: (0.5, -1.0),
+            7: (-0.5, -2.0),
+            8: (0.5, -2.0),
+        }
+
+        dot_coords = [
+            dot_to_coord_mapping_mode_6[dot_num]
+            for dot_num in dot_to_coord_mapping_mode_6
+            if (self.cell_int_repr >> (dot_num - 1)) & 1 > 0
+        ]
+
+        # If 8-dot mode, shift all north by 0.5.
+        if dots_per_cell_mode == 8:  # noqa: PLR2004
+            for i, (x, y) in enumerate(dot_coords):
+                dot_coords[i] = (x, y + 0.5)
+
+        return dot_coords
+
 
 @dataclass
 class BrailleStickSpec:
@@ -32,7 +221,7 @@ class BrailleStickSpec:
     )
 
     # Stick dimensions.
-    stick_face_width: float = 16.0
+    stick_face_width: float = 28.0
     stick_face_thickness: float = 2.0
     stick_corner_radius: float = 1.0
 
@@ -40,6 +229,8 @@ class BrailleStickSpec:
     dot_pitch_x: float = 2.5
     dot_pitch_y: float = 2.5
     cell_pitch_x: float = 6.0
+    dot_diameter: float = 1.5
+    dot_height: float = 1.0
 
     def __post_init__(self) -> None:
         """Post initialization checks."""
@@ -69,6 +260,22 @@ class BrailleStickSpec:
         return inscribed_radius(
             n=self.side_count, side_length=self.stick_face_width
         )
+
+    @property
+    def normalized_cells_each_face(self) -> list[list[BrailleCell]]:
+        """Get the normalized cells for each face."""
+        return [
+            [BrailleCell(cell) for cell in face]
+            for face in self.cells_each_face
+        ]
+
+    @property
+    def dots_per_cell_mode_each_face(self) -> list[Literal[6, 8]]:
+        """Get the dots per cell mode for each face."""
+        return [
+            max(cell.dot_count for cell in face)
+            for face in self.normalized_cells_each_face
+        ]
 
 
 def inscribed_radius(n: int, side_length: float) -> float:
@@ -122,8 +329,59 @@ def make_braille_stick(spec: BrailleStickSpec) -> bd.Part | bd.Compound:
         polygon_outline,
         amount=spec.total_length,
     )
-
     p += stick
+
+    # Find the faces of the stick, and draw braille on them (treating them as
+    # sketch surfaces).
+    # Select the side_count largest faces.
+    faces = sorted(stick.faces(), key=lambda f: f.area, reverse=True)
+    faces = faces[: spec.side_count]
+    assert len(faces) == spec.side_count, (
+        f"Expected {spec.side_count} faces, got {len(faces)}"
+    )
+    for _face_num, (face, normalized_cell_list, dot_mode) in enumerate(
+        zip(
+            faces,
+            spec.normalized_cells_each_face,
+            spec.dots_per_cell_mode_each_face,
+            strict=True,
+        )
+    ):
+        # Create a sketch on the face.
+        sketch = bd.Sketch(None)
+
+        # Draw the braille dots.
+        for cell_num, normalized_cell in enumerate(normalized_cell_list):
+            for (
+                dot_offset_x,
+                dot_offset_y,
+            ) in normalized_cell.get_raised_xy_point_list(
+                dots_per_cell_mode=dot_mode
+            ):
+                dot_x = (
+                    dot_offset_x * spec.dot_pitch_x
+                    + spec.cell_pitch_x * cell_num
+                    - (spec.cell_pitch_x * (spec.cell_count - 1) / 2)
+                )
+                dot_y = dot_offset_y * spec.dot_pitch_y
+
+                sketch += (
+                    bd.Plane(face)  # pyright: ignore[reportOperatorIssue]
+                    * bd.Pos((dot_y, -dot_x))
+                    * bd.Circle(
+                        radius=spec.dot_diameter / 2,
+                    )
+                )
+
+        # Extrude the sketch to create the braille dots.
+        # assert isinstance(sketch, bd.Compound)  # Type checking.
+        dot_extrude = bd.extrude(
+            sketch,  # pyright: ignore[reportArgumentType]
+            amount=spec.dot_height,
+            clean=True,
+        )
+        p += dot_extrude
+        logger.debug(f"Done with face {_face_num + 1} of {spec.side_count}")
 
     return p
 
@@ -142,8 +400,6 @@ if __name__ == "__main__":
         assert isinstance(part, bd.Part | bd.Solid | bd.Compound), (
             f"{name} is not an expected type ({type(part)})"
         )
-        if not part.is_manifold:
-            logger.warning(f"Part '{name}' is not manifold")
 
         bd.export_stl(part, str(export_folder / f"{name}.stl"))
         bd.export_step(part, str(export_folder / f"{name}.step"))
